121015843de60c5d2d1931cdf2efed7c7b8cdc39
Виктор Парыгин <paryginva@live.com>
2022-01-23 13:09:37 +0500

    Добавьте файлы проекта.

---
+++ WpfApp_Task19/Models/GeomCalc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Task19.Models
+{
+    static class GeomCalc
+    {
+        public static double CircumferenceByRadius(double a) => Math.Round(2 * Math.PI * a,3);
+    }
+}
---
+++ WpfApp_Task19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp_Task19.Models;
+
+namespace WpfApp_Task19.ViewModels
+{
+    class MainWindowViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double circumference;
+        public double Circumference
+        {
+            get => circumference;
+            set
+            {
+                circumference = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand GetCircumferenceByRadiusCommand { get; }
+        private void OnGetCircumferenceByRadiusCommandExecute(object p)
+        {
+            Circumference = GeomCalc.CircumferenceByRadius(Radius);
+        }
+        private bool CanGetCircumferenceByRadiusCommandExecuted(object p)
+        {
+            if (Radius != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            GetCircumferenceByRadiusCommand = new RelayCommand(OnGetCircumferenceByRadiusCommandExecute, CanGetCircumferenceByRadiusCommandExecuted);
+        }
+    }
+}
